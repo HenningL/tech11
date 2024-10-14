a6477872427658d7f1c951f296b652ff4629ff9c
Henning Luther <henning.luther@testifi.io>
2024-10-14 20:54:38 +0200

    fix init data for version
    Signed-off-by: Henning Luther <henning.luther@testifi.io>

--- jakartaee-hluther/src/main/resources/META-INF/initial-data.sql
+++ jakartaee-hluther/src/main/resources/META-INF/initial-data.sql
@@ -1,1 +1,1 @@
-INSERT INTO USERS (firstname, lastname,email,birthday,password) VALUES ('Henning','Luther', 'hluther80@gmail.com','1980-03-30','secret');
+INSERT INTO USERS (firstname, lastname,email,birthday,password,version) VALUES ('Henning','Luther', 'hluther80@gmail.com','1980-03-30','secret',1);
